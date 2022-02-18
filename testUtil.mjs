9d8bb1697959e22adb9a997336a2fc2f1b9a0b4a
Scott Gifford <sggiffor@amazon.com>
2022-02-17 22:43:06 -0500

    Convert to npm

--- testUtil.mjs
+++ testUtil.mjs
@@ -1,11 +1,13 @@
+#!/usr/bin/env node --experimental-modules
+
 import { charOccurrences, takeGuess } from "./util.mjs";
 
 console.assert(charOccurrences("abba", "a") == 2, "finds a 2");
 console.assert(charOccurrences("abba", "b") == 2, "finds b 2");
 console.assert(charOccurrences("abba", "c") == 0, "finds c 0");
 
-console.assert(takeGuess("abcde","fghij").join("") === "-----", "none matching");
-console.assert(takeGuess("abcde","abcde").join("") === "GGGGG", "all matching");
-console.assert(takeGuess("aaaaa","aaaaa").join("") === "GGGGG", "all same");
-console.assert(takeGuess("aaaaa","abbbb").join("") === "G----", "one in many");
-console.assert(takeGuess("balza","henna").join("") === "----G", "balza/henna");
+console.assert(takeGuess("abcde","fghij") === "-----", "none matching");
+console.assert(takeGuess("abcde","abcde") === "GGGGG", "all matching");
+console.assert(takeGuess("aaaaa","aaaaa") === "GGGGG", "all same");
+console.assert(takeGuess("aaaaa","abbbb") === "G----", "one in many");
+console.assert(takeGuess("balza","henna") === "----G", "balza/henna");
