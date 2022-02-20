5330ad813943f2db4bd0e4c0e06081e84f4284ff
Scott Gifford <sggiffor@amazon.com>
2022-02-19 21:41:42 -0500

    Add comments, options, etc. to "freq" strategy, and updated scores.

--- strategyFreqy.mjs
+++ strategyFreqy.mjs
@@ -4,6 +4,14 @@ import { FrequencyAnalysis } from "./freq.mjs";
 import { Logger } from "./log.mjs";
 
 const RIGHT_PLACE_MULTIPLIER = 3; // Determined experimentally, though doesn't seem to matter much
+const LOG_REMAINING_WORDS_THRESHOLD = 20; // When this many or fewer words or left, log the list of possibilties
+const LOG_TOP_N_SCORES = 10; // Log this many top scores
+
+/** Frequency analyzer strategy.
+ *
+ * Do a frequency analysis of all possible words, then score words based on the frequency analysis,
+ * and choose the highest score of a word which is a remaining possibility.
+ */
 export class StrategyFreqy extends Strategy {
     constructor(words, options) {
         super(words, options);
@@ -31,7 +39,7 @@ export class StrategyFreqy extends Strategy {
         if (this.remainingWords.length === 0) {
             throw new Error('No remaining possibilities, giving up');
         }
-        if (this.remainingWords.length < 20) {
+        if (this.remainingWords.length < LOG_REMAINING_WORDS_THRESHOLD) {
             Logger.log('strategy', 'info', this.remainingWords.map((word) => { return { word, score: this.scoreWord(word, this.leFreq) } }));
         }
 
@@ -41,10 +49,18 @@ export class StrategyFreqy extends Strategy {
         Logger.log('strategy', 'info', `Choosing best word from ${words.length} possibilities`);
         const scores = words.map(word => ({ word, score: this.scoreWord(word, freq)})).sort((a, b) => /* reverse sort */ b.score - a.score);
         Logger.log('strategy', 'info', `Choosing best score from ${scores.length} possibilities`);
-        Logger.log('score', 'debug', 'Top 10 Scores:', scores.slice(0,10));
+        Logger.log('score', 'debug', `Top ${LOG_TOP_N_SCORES} Scores:`, scores.slice(0,LOG_TOP_N_SCORES));
         return scores[0].word;
     }
 
+    /** Score a word with the given frequency table.
+     *
+     * For each letter in the given word, award one point for every time it occurs anywhere in the frequency table,
+     * and RIGHT_PLACE_MULTIPLIER points for every time it occurs at the gussed position in the frequency table.
+     * Only score each letter once when it is in a solution; additional occurrences are scored at 0
+     * (otherwise we would try to pick a word a word with as many frequently-seen letters as possible, e.g. "seres";
+     * disable this behavior by setting strategy option `scoreDuplicateLetters` to a truthy value.
+     */
     scoreWord(word, freq) {
         let score = 0;
         let picked = { };
@@ -57,7 +73,9 @@ export class StrategyFreqy extends Strategy {
             } else {
                 Logger.log('score', 'trace', `No score for letter ${i} in word ${word}`);
             }
-            picked[letter] = (picked[letter] || 0) + 1;
+            if (!this.options.scoreDuplicateLetters) {
+                picked[letter] = (picked[letter] || 0) + 1;
+            }
         }
         Logger.log('score', 'trace', `Score for ${word} is ${score}`);
         return score;
