08ea4fc11ca81d9fc522df138256b835e2dc3872
Scott Gifford <sggiffor@amazon.com>
2022-02-20 21:15:18 -0500

    Remove freq parameter from StrategyScoringAbstract class.

--- strategyScoringAbstract.mjs
+++ strategyScoringAbstract.mjs
@@ -43,22 +43,26 @@ export class StrategyScoringAbstract extends Strategy {
      * @param {score} word Word to score
      * @returns {WordWithScore} Word with score object
      */
-    wordWithScore(word, freq) {
+    wordWithScore(word) {
         throw new Error('Not implemented, abstract class');
     }
 
-    scoreAndSortWords(words, freq) {
-        return words.map(word => this.wordWithScore(word, freq)).sort((a, b) => {
+    scoreAndSortWords(words) {
+        return words.map(word => this.wordWithScore(word)).sort((a, b) => {
             return b.score - a.score  // Reverse sort, highest to lowest
         });
     }
 
-    bestWord(words, freq) {
-        const scores = this.scoreAndSortWords(words, freq);
+    bestWordWithScore(words) {
+        const scores = this.scoreAndSortWords(words);
         Logger.log('score', 'debug', `Top ${this.options.logTopNScores} Scores:`, scores.slice(0,this.options.logTopNScores).map((s) => JSON.stringify(s)).join(",\n"));
         if (this.options.extraScoreWords) {
             Logger.log('score', 'debug', 'Extra word scores:',this.scoreAndSortWords(this.options.extraScoreWords, freq).map((s) => JSON.stringify(s)).join(",\n"));
         }
-        return scores[0].word;
+        return scores[0];
+    }
+
+    bestWord(words) {
+        return this.bestWordWithScore(words).word;
     }
 }
