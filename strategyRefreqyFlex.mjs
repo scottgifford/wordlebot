0966ae60a5956040670a20db5f9f5ba412616773
Scott Gifford <sggiffor@amazon.com>
2022-02-20 21:18:14 -0500

    Refactor StrategyRefreqyFlex to be simpler and easier to extend.

--- strategyRefreqyFlex.mjs
+++ strategyRefreqyFlex.mjs
@@ -1,36 +1,71 @@
 import { Logger } from "./log.mjs";
+import { StrategyFreqy } from "./strategyFreqy.mjs";
 import { StrategyRefreqy } from "./strategyRefreqy.mjs";
 
-const SCORE_RATIO = 0.30;
+const DEFAULT_SCORE_RATIO = 0.30;
 
+/**
+ * Frequency-analyzer based strategy, with the flexibility to choose words that are not remaining possible
+ * solutions if the strategy decides they will reveal more information.
+ */
 export class StrategyRefreqyFlex extends StrategyRefreqy {
     constructor(words, options) {
-        super(words, options);
+        super(words, {
+            scoreRatio: DEFAULT_SCORE_RATIO,
+            ...options,
+        });
     }
 
+    /**
+     * Create a new frequency analysis based on the current one, but with modifications, used to choose the flex word.
+     *
+     * The base class removes entries from the original frequency analysis where we know for sure the letter will occur in the word
+     * (since we will not learn much from that).
+     *
+     * @returns Updated frequency analysis
+     */
+    // TODO: Does this really need to be a method?
     reFreq() {
         Logger.log('strategy', 'debug', `RefreqyFlex reFreq`);
         return this.leFreq.clone(([k, v]) => !this.letters.definitelyHasLetter(k));
     }
 
+    /**
+     *
+     * Chose the flex word for this strategy.
+     *
+     * @returns Word and score for flex word choice
+     */
+    chooseFlexWordAndScore() {
+        const flexWordSubStrategy = new StrategyFreqy(this.words, {
+            freq: this.reFreq(),
+        });
+
+        return flexWordSubStrategy.bestWordWithScore(this.words);
+    }
+
+    /**
+     * From the given flex word and remaining word, choose one and return it.
+     *
+     * @param {*} flexWordAndScore Flex word and score to consider
+     * @param {*} remainingWordAndScore Remaining word and score to consider
+     * @returns Either flex word or remaining word
+     */
+    chooseFlexOrRemainingWord(flexWordAndScore, remainingWordAndScore) {
+        const scoreRatio = flexWordAndScore.score / remainingWordAndScore.score;
+
+        const chosenWordAndScore = scoreRatio > this.options.scoreRatio ? flexWordAndScore : remainingWordAndScore;
+        Logger.log('strategy', 'debug', `Chose ${chosenWordAndScore.word} with score ratio ${scoreRatio} (vs. ${this.options.scoreRatio}); Flex word ${chosenWordAndScore.word} score ${chosenWordAndScore.score}; Best remaining word ${remainingWordAndScore.word} score ${remainingWordAndScore.score}`);
+        return chosenWordAndScore;
+    }
+
     guess() {
-        // Choose and score both a word from the set of remaining possibilities, and from the set of words that contain none of the existing letters
-        // As a (maybe nonsense?) heuristic, look at the ration of the overall choice to the remaining word choice,
-        // and if it is above a threshold, choose the overall choice instead.
-        // The threshold is kind of arbitrary and its meaning is not clear; but the remaining choice will usually be better because it's
-        // used up the most common (therefore higher scoring) letters.
-        const freq2 = this.reFreq();
-        const bestOverallWord = this.bestWord(this.words, freq2);
-        const bestOverallWordScore = bestOverallWord ? this.scoreWord(bestOverallWord, freq2) : 0;
-
-        const bestRemainingWord = this.bestWord(this.remainingWords, this.leFreq);
-        const bestRemainingWordScore = this.scoreWord(bestRemainingWord, this.leFreq);
-
-        const scoreRatio = bestOverallWordScore / bestRemainingWordScore;
-        
-        const chosenWord = scoreRatio > SCORE_RATIO ? bestOverallWord : bestRemainingWord;
-        Logger.log('strategy', 'info', `Chose ${chosenWord} with score ratio ${scoreRatio} (vs. ${SCORE_RATIO}); Best overall word ${bestOverallWord} score ${bestOverallWordScore}; Best remaining word ${bestRemainingWord} score ${bestRemainingWordScore}`);
-
-        return chosenWord;
+        // Choose and score both a word from the set of remaining possibilities, and from the set of words that contain none of the existing letters.
+        // Use chooseFlexOrRemainingWord() method to decide which of these to use.
+        const wordAndScore = this.chooseFlexOrRemainingWord(
+            this.chooseFlexWordAndScore(),
+            this.bestWordWithScore(this.remainingWords)
+        );
+        return wordAndScore.word;
     }
 }
