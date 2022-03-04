799d57c8c7fa2aef062d23f2805a8c2089070f9f
Scott Gifford <sggiffor@amazon.com>
2022-03-03 22:36:16 -0500

    Turn tournament options into command-line options.

--- strategyRefreqyFlexDoubleRulesTournament.mjs
+++ strategyRefreqyFlexDoubleRulesTournament.mjs
@@ -1,22 +1,48 @@
 import { Logger } from "./log.mjs";
+import { StrategyOption, StrategyOptionInteger } from "./strategy.mjs";
 import { StrategyRefreqyFlexDoubleRules } from "./strategyRefreqyFlexDoubleRules.mjs";
 import { StrategySimGuessMinList } from "./strategySimGuessMinList.mjs";
 
-const TOP_N_WORDS = 100;
-const MAX_GUESS_SIMS = 50;
-const MAX_SOLUTION_SIMS = 50;
+const DEFAULT_INITIAL_GUESS = 'tares';
 
 export class StrategyRefreqyFlexDoubleRulesTournament extends StrategyRefreqyFlexDoubleRules {
+    getSupportedOptions() {
+        return [
+            new StrategyOptionInteger(
+                'topWords', 100,
+                'Choose this many top words to send to simulation'),
+            new StrategyOptionInteger(
+                'maxSolutionSims', 50,
+                'Chose at most this many solution simulations (-1 to ignore and use ratio)'),
+            new StrategyOptionInteger(
+                'maxGuessSims', 50,
+                'Chose at most this many guess simulations (-1 to ignore and use ratio)'),
+            new StrategyOption(
+                'useInitialGuess', true,
+                'Use a pre-calculated word for our first guess instead of simulating'),
+            new StrategyOption(
+                'initialGuess', DEFAULT_INITIAL_GUESS,
+                'Use a pre-calculated word for our first guess instead of simulating'),
+            new StrategyOption(
+                'samplingRandom', true,
+                'Use a pre-calculated word for our first guess instead of simulating'),
+
+            ...super.getSupportedOptions()
+        ];
+    }
+
     chooseFlexWordAndScore() {
         const flexWordSubStrategy = this.newSubStrategy(this.words, this.flexFreq());
         const bestWords = flexWordSubStrategy.scoreAndSortWords(this.words);
-        const topWords = bestWords.slice(0, TOP_N_WORDS).map(ws => ws.word);
+        const topWords = bestWords.slice(0, this.options.topWords).map(ws => ws.word);
 
         Logger.log('strategy','debug','Starting tournament strategy with words', topWords);
         const tournamentStrategy = new StrategySimGuessMinList(this.remainingWords, {
-            maxGuessSims: MAX_GUESS_SIMS,
-            maxSolutionSims: MAX_SOLUTION_SIMS,
+            maxSolutionSims: this.options.maxSolutionSims,
+            maxGuessSims: this.options.maxGuessSims,
             letters: this.letters.clone(),
+            useInitialGuess: false,
+            samplingRandom: false,
         });
 
         tournamentStrategy.scoreCache = { }; // TODO: Move to method or something
